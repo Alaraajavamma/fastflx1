58170630426aad40fc0ea7a1a527bdf6ac5a5c25
Alaraajavamma <aki.siikaaho@gmail.com>
2024-12-11 15:25:26 +0000

    Add new file

---
+++ uninstall.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+definately not ready lol
+
+rm ~/.local/share/applications/fastdial.desktop
+rm ~/.config/autostart/fasthistory.desktop
+
+cd ~ && rm -rf .git/fastcalls
+
+pkexec rm -rf /opt/fastcalls
