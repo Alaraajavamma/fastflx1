af79d35dd742f2b185dbd83444308eb07fc19ba5
Alaraajavamma <aki.siikaaho@gmail.com>
2024-12-14 19:53:28 +0000

    Update uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 rm ~/.local/share/applications/fastdial.desktop
+rm ~/.local/share/applications/yad-icon-browser.desktop
 rm ~/.config/autostart/fasthistory.desktop
 rm ~/.config/gtk-3.0/gtk.css
 rm -rf ~/.config/feedbackd
@@ -11,3 +12,4 @@ rm -rf ~/.local/share/squeekboard
 cd ~ && rm -rf .git/fastflx1
 
 pkexec rm -rf /opt/fastflx1
+
