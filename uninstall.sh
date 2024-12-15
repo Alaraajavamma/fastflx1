3ae61d8ab0a400ee0e360f12adee4c3fe9da6b22
Alaraajavamma <aki.siikaaho@gmail.com>
2024-12-15 08:12:06 +0000

    Update uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 rm ~/.local/share/applications/fastdial.desktop
 rm ~/.local/share/applications/yad-icon-browser.desktop
 rm ~/.config/autostart/fasthistory.desktop
@@ -7,9 +6,15 @@ rm ~/.config/gtk-3.0/gtk.css
 rm -rf ~/.config/feedbackd
 rm -rf ~/.config/assistant-button
 rm -rf ~/.local/share/squeekboard
-
-
+sudo rm /usr/bin/alarmvol
+sudo rm /usr/bin/dialtone
+sudo rm /usr/bin/double-press
+sudo rm /usr/bin/fastflx1
+sudo rm /usr/bin/gnome-weather-location
+sudo rm /usr/bin/long-press
+sudo rm /usr/bin/short-press
+sudo rm /usr/bin/squeekboard-scale
+sudo apt remove yad
+gsettings set org.gnome.desktop.sound theme-name default
 cd ~ && rm -rf .git/fastflx1
-
-pkexec rm -rf /opt/fastflx1
-
+sudo rm -rf /opt/fastflx1
