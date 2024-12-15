338598446d14148a766d92a44edbe570201bbc32
Alaraajavamma <aki.siikaaho@gmail.com>
2024-12-15 11:22:45 +0000

    Replace update.sh

--- update.sh
+++ update.sh
@@ -1,3 +1,23 @@
 #!/bin/bash
+rm ~/.local/share/applications/fastflx1.desktop
+rm ~/.local/share/applications/yad-icon-browser.desktop
+rm ~/.config/autostart/dialtone.desktop
+rm ~/.config/autostart/alarmvol.desktop
+rm ~/.config/gtk-3.0/gtk.css
+rm -rf ~/.config/feedbackd
+rm -rf ~/.config/assistant-button
+rm -rf ~/.local/share/squeekboard
+sudo rm /usr/bin/alarmvol
+sudo rm /usr/bin/dialtone
+sudo rm /usr/bin/double-press
+sudo rm /usr/bin/fastflx1
+sudo rm /usr/bin/gnome-weather-location
+sudo rm /usr/bin/long-press
+sudo rm /usr/bin/short-press
+sudo rm /usr/bin/squeekboard-scale
+sudo apt remove yad
+gsettings set org.gnome.desktop.sound theme-name default
+cd ~ && rm -rf .git/fastflx1
+sudo rm -rf /opt/fastflx1
 
-cd ~/.git/fastflx1/ && git fetch && git reset --hard HEAD && git merge @{u}
+sudo apt install git && cd ~ && mkdir -p .git && cd .git && git clone https://gitlab.com/Alaraajavamma/fastflx1 && cd fastflx1 && sudo chmod +x install.sh && ./install.sh 
