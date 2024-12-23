004a90df8dba0ba1393def322fcd3ef461db35db
Alaraajavamma <aki.siikaaho@gmail.com>
2024-12-23 20:51:28 +0000

    Replace uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 rm ~/.local/share/applications/fastflx1.desktop
 rm ~/.local/share/applications/yad-icon-browser.desktop
+rm ~/.local/share/applications/display-im6.q16.desktop
 rm ~/.config/autostart/dialtone.desktop
 rm ~/.config/autostart/alarmvol.desktop
+rm ~/.config/autostart/gen-thumbnails.desktop
 rm ~/.config/gtk-3.0/gtk.css
 rm -rf ~/.config/feedbackd
 rm -rf ~/.config/assistant-button
@@ -15,7 +17,6 @@ sudo rm /usr/bin/gnome-weather-location
 sudo rm /usr/bin/long-press
 sudo rm /usr/bin/short-press
 sudo rm /usr/bin/squeekboard-scale
-sudo apt remove yad wl-clipboard
+sudo apt remove yad wl-clipboard inotify-tools imagemagick
 gsettings set org.gnome.desktop.sound theme-name default
 cd ~ && rm -rf .git/fastflx1
-sudo rm -rf /opt/fastflx1
