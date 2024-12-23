8983c9b1f2b724eea2f02b243cd7d43c5e7dcb9f
Alaraajavamma <aki.siikaaho@gmail.com>
2024-12-23 20:53:16 +0000

    Replace update.sh

--- update.sh
+++ update.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 rm ~/.local/share/applications/fastflx1.desktop
 rm ~/.local/share/applications/yad-icon-browser.desktop
+rm ~/.local/share/applications/display-im6.q16.desktop
 rm ~/.config/autostart/dialtone.desktop
 rm ~/.config/autostart/alarmvol.desktop
+rm ~/.config/autostart/gen-thumbnails.desktop
 rm ~/.config/gtk-3.0/gtk.css
 rm -rf ~/.config/feedbackd
 rm -rf ~/.config/assistant-button
@@ -10,12 +12,12 @@ rm -rf ~/.local/share/squeekboard
 sudo rm /usr/bin/alarmvol
 sudo rm /usr/bin/dialtone
 sudo rm /usr/bin/double-press
+sudo rm /usr/bin/gen-thumbnails
 sudo rm /usr/bin/fastflx1
 sudo rm /usr/bin/gnome-weather-location
 sudo rm /usr/bin/long-press
 sudo rm /usr/bin/short-press
 sudo rm /usr/bin/squeekboard-scale
 cd ~ && rm -rf .git/fastflx1
-sudo rm -rf /opt/fastflx1
 
 sudo apt install git && cd ~ && mkdir -p .git && cd .git && git clone https://gitlab.com/Alaraajavamma/fastflx1 && cd fastflx1 && sudo chmod +x install.sh && ./install.sh 
