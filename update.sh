92cce59d571739d975b1ff60818fbc86b93dae72
Alaraajavamma <aki.siikaaho@gmail.com>
2024-12-28 11:01:14 +0000

    Replace update.sh

--- update.sh
+++ update.sh
@@ -6,6 +6,7 @@ rm ~/.config/autostart/dialtone.desktop
 rm ~/.config/autostart/alarmvol.desktop
 rm ~/.config/autostart/gen-thumbnails.desktop
 rm ~/.config/autostart/gesture-shortcuts.desktop
+rm ~/.config/autostart/vol-buttons.desktop
 rm ~/.config/gtk-3.0/gtk.css
 rm -rf ~/.config/feedbackd
 rm -rf ~/.config/assistant-button
@@ -21,6 +22,7 @@ sudo rm /usr/bin/gnome-weather-location
 sudo rm /usr/bin/long-press
 sudo rm /usr/bin/short-press
 sudo rm /usr/bin/squeekboard-scale
+sudo rm /usr/bin/vol-buttons
 cd ~ && rm -rf .git/fastflx1
 
 sudo apt install git && cd ~ && mkdir -p .git && cd .git && git clone https://gitlab.com/Alaraajavamma/fastflx1 && cd fastflx1 && sudo chmod +x install.sh && ./install.sh 
