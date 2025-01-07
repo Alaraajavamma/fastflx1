99e553081945084e940e900098f403da1c7014f7
Alaraajavamma <aki.siikaaho@gmail.com>
2025-01-07 10:44:18 +0000

    Edit install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sudo apt install -y wtype curl wl-clipboard inotify-tools imagemagick lisgd evtest libcallaudio-tools wofi
+sudo apt install -y wtype curl wl-clipboard inotify-tools imagemagick lisgd evtest libcallaudio-tools wofi feh
 
 # Set permissions for uninstall and update scripts
 sudo chmod +x "${HOME}/.git/fastflx1/uninstall.sh" "${HOME}/.git/fastflx1/update.sh"
