d8c24092fbe73df6c87c603ab5ec3f8b18d7a150
Alaraajavamma <aki.siikaaho@gmail.com>
2025-08-03 13:59:24 +0000

    Edit update.sh

--- update.sh
+++ update.sh
@@ -1,32 +1,7 @@
 #!/bin/bash
-rm ~/.local/share/applications/fastflx1.desktop
-rm ~/.local/share/applications/yad-icon-browser.desktop
-rm ~/.local/share/applications/display-im6.q16.desktop
-rm ~/.local/share/applications/display-im7.q16.desktop
-rm ~/.local/share/applications/feh.desktop
-rm ~/.config/autostart/dialtone.desktop
-rm ~/.config/autostart/alarmvol.desktop
-rm ~/.config/autostart/gesture-shortcuts.desktop
-rm ~/.config/autostart/vol-buttons.desktop
-rm ~/.config/autostart/evremap.desktop
-rm ~/.config/gtk-3.0/gtk.css
-rm -rf ~/.config/wofi/
-rm -rf ~/.config/feedbackd
-rm -rf ~/.config/evremap
-rm -rf ~/.config/assistant-button
-rm -rf ~/.local/share/squeekboard
-rm -rf ~/.local/share/sounds
-sudo rm /usr/bin/alarmvol
-sudo rm /usr/bin/dialtone
-sudo rm /usr/bin/double-press
-sudo rm /usr/bin/fastflx1
-sudo rm /usr/bin/gnome-weather-location
-sudo rm /usr/bin/gesture-shortcuts
-sudo rm /usr/bin/long-press
-sudo rm /usr/bin/short-press
-sudo rm /usr/bin/squeekboard-scale
-sudo rm /usr/bin/vol-buttons
-sudo rm /usr/bin/assistant-button-tweak
-cd ~ && rm -rf .git/fastflx1
+
+"${HOME}/.git/fastflx1/uninstall.sh"
+
+echo "Uninstall script finished."
 
 sudo apt install git && cd ~ && mkdir -p .git && cd .git && git clone https://gitlab.com/Alaraajavamma/fastflx1 && cd fastflx1 && sudo chmod +x install.sh && ./install.sh 
