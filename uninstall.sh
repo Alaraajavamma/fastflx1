6a5bafbb4c697660bc9a338cfab16fc88959679f
Alaraajavamma <aki.siikaaho@gmail.com>
2025-08-03 13:52:22 +0000

    Edit uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -1,14 +1,10 @@
 #!/bin/bash
 rm ~/.local/share/applications/fastflx1.desktop
-rm ~/.local/share/applications/yad-icon-browser.desktop
-rm ~/.local/share/applications/display-im6.q16.desktop
-rm ~/.local/share/applications/display-im7.q16.desktop
-rm ~/.local/share/applications/feh.desktop
 rm ~/.config/autostart/dialtone.desktop
 rm ~/.config/autostart/alarmvol.desktop
 rm ~/.config/autostart/gesture-shortcuts.desktop
-rm ~/.config/autostart/vol-buttons.desktop
-rm ~/.config/autostart/evremap.desktop
+rm ~/.config/autostart/andromeda-guard.desktop
+rm ~/.config/autostart/batterysaver.desktop
 rm ~/.config/gtk-3.0/gtk.css
 rm -rf ~/.config/wofi/
 rm -rf ~/.config/feedbackd
@@ -18,6 +14,9 @@ rm -rf ~/.local/share/squeekboard
 rm -rf ~/.local/share/sounds
 sudo rm /usr/bin/alarmvol
 sudo rm /usr/bin/dialtone
+sudo rm /usr/bin/batterysaver
+sudo rm /usr/bin/andromeda-guard
+sudo rm /usr/bin/andromeda-shared-folders
 sudo rm /usr/bin/double-press
 sudo rm /usr/bin/fastflx1
 sudo rm /usr/bin/gnome-weather-location
@@ -25,8 +24,6 @@ sudo rm /usr/bin/gesture-shortcuts
 sudo rm /usr/bin/long-press
 sudo rm /usr/bin/short-press
 sudo rm /usr/bin/squeekboard-scale
-sudo rm /usr/bin/vol-buttons
-sudo rm /usr/bin/assistant-button-tweak
-sudo apt remove yad wl-clipboard inotify-tools imagemagick lisgd evtest libcallaudio-tools wofi feh libnotify-bin evremap
+sudo apt remove wl-clipboard lisgd libcallaudio-tools wofi
 gsettings set org.gnome.desktop.sound theme-name default
 cd ~ && rm -rf .git/fastflx1
