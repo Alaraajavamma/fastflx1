168f5becde1522b8dcaea98bc4c86e4742bef11c
Alaraajavamma <aki.siikaaho@gmail.com>
2025-08-13 18:09:24 +0000

    Delete update.sh

--- update.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-"${HOME}/.git/fastflx1/uninstall.sh"
-
-echo "Uninstall script finished."
-
-sudo apt install git && cd ~ && mkdir -p .git && cd .git && git clone https://gitlab.com/Alaraajavamma/fastflx1 && cd fastflx1 && sudo chmod +x install.sh && ./install.sh 
